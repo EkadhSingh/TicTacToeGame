b356a77a6fe93a655e10e154de5af514c9d61cae
EkadhSingh <56805938+EkadhSingh@users.noreply.github.com>
2024-08-31 20:35:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,9 +10,9 @@ public class Main {
 		int[] boardToCheck = {2,4,13,15,1,3,14,16};
 		//For the output the first 4 numbers should be the position of the x pieces, the next 4 the position of the o pieces, the 9th number should be who's turn it is to move, with 0 being x and 1 being o, and the 10th number in the output is who's winning this position, with 0 meaning it's a draw, 1 meaning x has a guaranteed win, 2 meaning o has a guaranteed win, and 3 meaning the position is impossible as both pieces have 3 in a row.
 		System.out.println("The board we are checking is:");
-		boards.print900900Boards(boards.peiceSpotsToBoardNumber(boardToCheck[0],boardToCheck[1],boardToCheck[2],boardToCheck[3],boardToCheck[4],boardToCheck[5],boardToCheck[6],boardToCheck[7]));
-		//If one side is winning and it is their move, this will print all of the possible moves (or technically, all of the possible boards they can go) to win. If neither side is winning, it will print all possible moves that don't lose.
-		boards.bestMoves(boards.peiceSpotsToBoardNumber(boardToCheck[0],boardToCheck[1],boardToCheck[2],boardToCheck[3],boardToCheck[4],boardToCheck[5],boardToCheck[6],boardToCheck[7]));
+		boards.print900900Boards(boards.peiceSpotsToBoardNumber(boardToCheck));
+		//Print every possible board
+		boards.printPossibleMoves(boards.peiceSpotsToBoardNumber(boardToCheck));
 
 	}
 
